85b3d7b24c89477926bd4e83d7229cde5338eb98
Putra Rizqi Pratama_213040080 <96675374+PutraBadooel@users.noreply.github.com>
2023-10-25 17:36:22 +0700

    Add files via upload

---
+++ prakweb2023_C_213040080_mvc/app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2023_C_213040078_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2023');
+
+
+?>
